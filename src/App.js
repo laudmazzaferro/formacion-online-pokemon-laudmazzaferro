32f04e34d9c4ade15a601d9a1ba36ef480814f77
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-10-21 17:39:01 +0200

    import pokemonlist and filter components , and create html strcuture for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import Filter from './components/Filter';
+import PokemonsList from './components/PokemonsList';
 
-function App() {
-  return (
-    <div className="App">
-      hola
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      pokemons:[],
+      inputPokemon: ''
+    }
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <header className="app_header">
+          <h1 className="app__title">Pokemons List</h1>
+        </header>
+        <main className="app__main">
+          <Filter />
+          <PokemonsList />
+        </main>
+        <footer className="app__footer">
+  
+        </footer>
+      </div>
+    );
+  }
 }
+
 
 export default App;
