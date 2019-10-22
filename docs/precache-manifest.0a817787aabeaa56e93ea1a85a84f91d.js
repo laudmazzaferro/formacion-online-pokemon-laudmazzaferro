55483193341bfe776e9ad48dc517ef320409c126
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-10-22 23:42:09 +0200

    buibuild docs

---
+++ docs/precache-manifest.0a817787aabeaa56e93ea1a85a84f91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da7d7d0a8cc3809e3a97d1a41ec6c22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b508578cede7ecdb8c2c",
+    "url": "./static/css/main.ed5c544d.chunk.css"
+  },
+  {
+    "revision": "2e1eded58fd1079e3d87",
+    "url": "./static/js/2.79f769c0.chunk.js"
+  },
+  {
+    "revision": "b508578cede7ecdb8c2c",
+    "url": "./static/js/main.b0580b3b.chunk.js"
+  },
+  {
+    "revision": "e618a3f0ae301f64d963",
+    "url": "./static/js/runtime-main.c9b19b08.js"
+  },
+  {
+    "revision": "d7a0d99ab8f7833ad4a719d205d3d43d",
+    "url": "./static/media/logo-pokemon.d7a0d99a.png"
+  }
+]);
