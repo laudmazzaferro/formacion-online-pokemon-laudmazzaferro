c351af6312609f8eab41a71f94e16fc2bb7fd473
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-10-22 22:19:33 +0200

    add style to types

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,23 +4,28 @@ import './../styles/Pokemon.scss';
 
 
 class Pokemon extends React.Component {
- 
+
   render() {
-    const {pokemon}=this.props;
+    const { pokemon } = this.props;
     return (
-    <div>
-      <h2 className="pokemon__name">{pokemon.name}</h2>
-      <p className="pokemon__id">{pokemon.id}</p>
-      <img className="pokemon__img" src={pokemon.sprites.front_default} alt=""/>
-      <ul className="pokemon__types--list">
-      {pokemon.types.map(item=>{
-        return(
-          <li key={item.type.name} className="pokemon__type--item"> {item.type.name}</li>
-        );
-      })}
-      </ul>
-      
-    </div>
+      <div>
+        <div className="photo-id__container">
+          <p className="pokemon__id">ID/{pokemon.id}</p>
+          <img className="pokemon__img" src={pokemon.sprites.front_default} alt="" />
+        </div>
+        <div className="name-type__container">
+          <h2 className="pokemon__name">{pokemon.name}</h2>
+          <ul className="pokemon__types--list">
+            {pokemon.types.map(item => {
+              return (
+                <li key={item.type.name} className="pokemon__type--item"> {item.type.name}</li>
+              );
+            })}
+          </ul>
+
+        </div>
+
+      </div>
     )
   }
 }
